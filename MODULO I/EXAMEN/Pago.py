caea7adacd7eb4f11c6f0ca79c065f98bc9371a5
Humberto Antonio Galdámez Chávez <hertzchavez@gmail.com>
2021-06-17 11:30:30 -0600

    Update Pago.py
    Se ha actualizado el metodo str para mostrar en un mejor formato la salida

--- MODULO I/EXAMEN/Pago.py
+++ MODULO I/EXAMEN/Pago.py
@@ -2,10 +2,7 @@ class Pago:
     def __init__(self, idpago:int, monto:float, fecha:str):
         self.__idpago = idpago
         self.__monto = monto
-        self.__fecha = str
+        self.__fecha = fecha
 
     def __str__(self):
-        return f""" Id Pago: {self.__idpago}
-                    Monto: {self.__monto}
-                    Fecha: {self.__fecha}
-                """
+        return "ID Pago : {:<10}  \nMonto : ${:<10.2f}  \nFecha : {:<12}".format(self.__idpago,self.__monto,self.__fecha)
